c46078c539d582990391b390e4df6039d070dcdd
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-01-04 21:08:14 +0000

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,16 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = undefined;
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    this._floors = value;
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = floors;
   }
 
   evacuationWarningMessage() {
