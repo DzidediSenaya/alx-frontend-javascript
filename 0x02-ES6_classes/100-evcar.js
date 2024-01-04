9ad7b2f99a60b6681100b0eae93b6e88bfc86b02
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-01-04 20:50:12 +0000

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,4 @@
-// 100-evcar.js
 import Car from './10-car';
-
-const cloneSymbol = Symbol('clone');
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -9,15 +6,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  [cloneSymbol]() {
-    const clonedCar = new Car(); // Create a Car instance instead of EVCar
-    Object.getOwnPropertyNames(this).forEach((prop) => {
-      clonedCar[prop] = this[prop];
-    });
-    return clonedCar;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
