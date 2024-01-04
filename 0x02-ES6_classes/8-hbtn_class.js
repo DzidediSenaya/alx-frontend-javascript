d221d663591a63e80f368b119ed86116e852426c
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-01-04 21:19:36 +0000

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,34 +1,18 @@
 export default class HolbertonClass {
-  _size;
-  _location;
+  size;
+  location;
 
   constructor(size, location) {
     this.size = size;
     this.location = location;
   }
 
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    this._location = value;
-  }
-
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
-      return this._size;
+      return this.size;
     }
     if (hint === 'string') {
-      return this._location;
+      return this.location;
     }
     return this;
   }
