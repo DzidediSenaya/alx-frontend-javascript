1c881fd8240d352cf580eb7f3d65dc6bfecbbff0
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-01-08 22:36:19 +0000

    reverted 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,15 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-export const queryAPI = (endpoint) => {
-  if (weakMap.has(endpoint)) {
-    const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 4) {
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, (endpointData + 1));
-  } else {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, count + 1);
   }
-};
+}
+
+export { queryAPI, weakMap };
