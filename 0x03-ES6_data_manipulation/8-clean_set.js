96ef319be6739e5fdd4bb485d98a18e5b987f305
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-01-09 00:53:54 +0000

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 function cleanSet(set, startString) {
   const filteredValues = [...set].filter(value => value.startsWith(startString));
 
+  if (startString === '') {
+    return filteredValues.join('-');
+  }
+
   return filteredValues.join('-').slice(startString.length);
 }
 
